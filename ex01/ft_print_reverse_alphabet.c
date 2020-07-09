6728894c9d9db47e8315ca1d841b0379c1b4256f
lakazzie87 <lakazzie@student.wethinkcode.co.za>
2020-07-09 15:34:41 +0200

    ex01/ft_print_reverse_alphabet.c

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,16 +6,13 @@
 /*   By: lakazzie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 15:47:58 by lakazzie          #+#    #+#             */
-/*   Updated: 2020/07/08 15:54:06 by lakazzie         ###   ########.fr       */
+/*   Updated: 2020/07/09 15:19:30 by lakazzie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putchar(char c)
-{
-	write(1, &c,1);
-}
+int ft_putchar(char c);
 
 void	ft_print_reverse_alphabet(void)
 {
